3f90a48a132fb40e416aa30a4a2b438b640e1945
Pavidha Rajendran <pavidha.rajendran@ifsworld.com>
2019-09-25 17:25:58 +0530

    Added Splash screen and changed color theme

---
+++ tripapp/app/src/main/java/com/example/tripapp/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.example.tripapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends Activity {
+    private ProgressBar progressBar;
+    int progress;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        progressBar=(ProgressBar)findViewById(R.id.progress);
+        Thread thred = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                doWork();
+                startApp();
+            }});
+        thred.start();
+    }
+    public void doWork() {
+        for (progress=10;progress<100;progress=progress+20){
+            try {
+                Thread.sleep(1000);
+                progressBar.setProgress(progress);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }} }
+    public void startApp(){
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+        finish();
+    }}
